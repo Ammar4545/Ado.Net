579d8da494a66bf79b18d6affbc5d9d77c18747f
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-03-30 23:18:28 +0200

    testing the project

---
+++ Connection/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Console.WriteLine(configuration.GetConnectionString("default")); 
+        }
+    }
+}
