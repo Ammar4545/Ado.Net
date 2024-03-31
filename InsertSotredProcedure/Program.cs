250198581cdb5ff52994b36d5d951b0229cedc5e
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-03-31 11:22:48 +0200

    change values with Update statment

---
+++ InsertSotredProcedure/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+namespace InsertSotredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var walletToInsert = new Wallet
+            {
+                Holder = "abrar",
+                Balance = 2000
+            };
+
+            //create connection to database
+            SqlConnection connection = new SqlConnection(configuration.GetConnectionString("default"));
+            //sql statment
+            var sql = "Insert into Wallets (Holder, Balance) Values (@Holder,@Balance);" +
+                $"SELECT CAST(scope_identity() AS int)";
+            //Create parameter
+            SqlParameter holderParameter = new SqlParameter
+            {
+                ParameterName = "@Holder",
+                SqlDbType = SqlDbType.VarChar,
+                Value = walletToInsert.Holder,
+                Direction = ParameterDirection.Input
+            };
+
+            SqlParameter balanceParameter = new SqlParameter
+            {
+                ParameterName = "@Balance",
+                SqlDbType = SqlDbType.Decimal,
+                Value = walletToInsert.Balance,
+                Direction = ParameterDirection.Input
+            };
+
+            SqlCommand command = new SqlCommand("AddWallet", connection);
+
+            command.Parameters.Add(holderParameter);
+            command.Parameters.Add(balanceParameter);
+            command.CommandType = CommandType.StoredProcedure;
+
+            connection.Open();
+
+            walletToInsert.Id = (int)command.ExecuteScalar();
+
+            Console.WriteLine($"Wallet for {walletToInsert.Holder} was added");
+
+            //if (command.ExecuteNonQuery() > 0)
+            //{
+            //    Console.WriteLine($"Wallet for {walletToInsert.Holder} was added");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Wallet for {walletToInsert.Holder} was not added");
+            //}
+
+            connection.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ UpdateRawSql/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+namespace UpdateRawSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            var walletToInsert = new Wallet
+            {
+                Holder = "testing..",
+                Balance = 2000
+            };
+
+            //create connection to database
+            SqlConnection connection = new SqlConnection(configuration.GetConnectionString("default"));
+            //sql statment
+            var sql = "UPDATE Wallets SET Holder =@Holder, Balance=@Balance WHERE Id=@Id";
+            //Create parameter
+            SqlParameter idParameter = new SqlParameter
+            {
+                ParameterName = "@Id",
+                SqlDbType = SqlDbType.Int,
+                Value = 1,
+                Direction = ParameterDirection.Input
+            };
+
+            SqlParameter holderParameter = new SqlParameter
+            {
+                ParameterName = "@Holder",
+                SqlDbType = SqlDbType.VarChar,
+                Value = walletToInsert.Holder,
+                Direction = ParameterDirection.Input
+            };
+
+            SqlParameter balanceParameter = new SqlParameter
+            {
+                ParameterName = "@Balance",
+                SqlDbType = SqlDbType.Decimal,
+                Value = walletToInsert.Balance,
+                Direction = ParameterDirection.Input
+            };
+
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.Add(idParameter);
+            command.Parameters.Add(holderParameter);
+            command.Parameters.Add(balanceParameter);
+
+            command.CommandType = CommandType.Text;
+
+            connection.Open();
+
+            //walletToInsert.Id = (int)command.ExecuteScalar();
+
+            //Console.WriteLine($"Wallet for {walletToInsert.Holder} was updated");
+
+            if (command.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine($"Wallet for {walletToInsert.Holder} was updated");
+            }
+            
+
+            connection.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
